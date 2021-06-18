a189019bb7408feab6539c854b2060924bc9a308
alex-k1 <33363821+alex-k1@users.noreply.github.com>
2021-06-17 21:35:59 -0500

    add custom validation messages; return validated and sanitized body

--- src/clojure_ring_reitit_datahike_realworld_example_app/handlers.clj
+++ src/clojure_ring_reitit_datahike_realworld_example_app/handlers.clj
@@ -1,26 +1,21 @@
 (ns clojure-ring-reitit-datahike-realworld-example-app.handlers
   (:require [clojure-ring-reitit-datahike-realworld-example-app.validations :as v]
-            [clojure.spec.alpha :as s]
-            [clojure.spec.gen.alpha :as gen]
             [ring.util.http-response :refer [ok unprocessable-entity]]))
 
-(defn login-user-service [conn user]
-  (gen/generate (s/gen :clojure-ring-reitit-datahike-realworld-example-app.specs/login-user-response))
-  nil)
+(defn- format-errors [errors]
+  (->> errors
+       (map (fn [[field message]] [field [message]]))
+       (into {})))
 
-;; TODO clean body
 (defn login-user [request]
-  (let [body (get-in request [:parameters :body])]
-    (if-let [errors (v/validate-body body v/login-user-schema)]
-      (unprocessable-entity {:errors errors})
-      (if-let [user (login-user-service (:conn request) body)]
+  (let [body (get-in request [:parameters :body])
+        login-user-svc (get-in request [:services :login-user-svc])
+        [errors valid-body] (v/validate-body body v/login-user-schema)]
+    (if (nil? errors)
+      (if-let [user (login-user-svc valid-body)]
         (ok {:user user})
-        (unprocessable-entity {:errors {:body {"email or password" ["is invalid"]}}})))))
-
-;; {"errors":
-;;  {"email": ["has already been taken"],
-;;   "password":["is too short (minimum is 8 characters)"],
-;;   "username":["has already been taken"]}}
+        (unprocessable-entity {:errors {"email or password" ["is invalid"]}}))
+      (unprocessable-entity {:errors (format-errors errors)}))))
 
 (defn register-user [request])
 
@@ -31,9 +26,17 @@
 (comment
 
   (login-user {:parameters {:body {:email "123asd.com" :password "6900"}}})
-  ;; => {:status 422, :headers {}, :body {:errors {:email ["must be a valid email"], :password ["less than the minimum 8"]}}}
+;; => {:status 422, :headers {}, :body {:errors {:email ["is invalid"], :password ["is too short (minimum is 8 character)"]}}}
 
-  (login-user {:parameters {:body {:email "123@asd.com" :password "124556900"}}})
-  ;; => {:status 200, :headers {}, :body {:user {:user {:email "8TPN013aS0lWg39J80ju4iF6", :token "1V", :username "36W2Cx3KwmefII3L", :bio nil, :image "L3JYp76AmJ1KTt"}}}}
+  (require '[clojure.spec.alpha :as s]
+           '[clojure.spec.gen.alpha :as gen])
+
+  (login-user {:parameters {:body {:email "123@asd.com" :password "124556900"}}
+               :services {:login-user-svc (constantly (gen/generate (s/gen :clojure-ring-reitit-datahike-realworld-example-app.specs/user)))}})
+;; => {:status 200, :headers {}, :body {:user {:email "nIUoi", :token "Z36o74FF2u3", :username "165PQNSOw1lTKU0R6e9K3uF8Cy", :bio "DH400me2gW3", :image "x69492F34"}}}
+
+  (login-user {:parameters {:body {:email "123@asd.com" :password "124556900"}}
+               :services {:login-user-svc (constantly nil)}})
+;; => {:status 422, :headers {}, :body {:errors {"email or password" ["is invalid"]}}}
 
   )
--- src/clojure_ring_reitit_datahike_realworld_example_app/validations.clj
+++ src/clojure_ring_reitit_datahike_realworld_example_app/validations.clj
@@ -1,34 +1,53 @@
 (ns clojure-ring-reitit-datahike-realworld-example-app.validations
-  (:require [struct.core :as st]))
+  (:require [clojure.string :as s]
+            [struct.core :as st]))
 
-;; TODO add custom error messages
-(def login-user-schema [[:email st/required st/email]
-                        [:password st/required [st/min-count 8] [st/max-count 100]]])
+;; use intermedium coerce step to sanitize input
+(def login-user-schema [[:email
+                         [st/required :message "can't be blank"]
+                         {:coerce s/trim}
+                         [st/email :message "is invalid"]]
+                        [:password
+                         [st/required :message "can't be blank"]
+                         {:coerce s/trim}
+                         [st/min-count 8 :message "is too short (minimum is %s character)"]
+                         [st/max-count 100 :message "is too long (maximum is %s character)"]]])
 
-(defn validate-body [body schema]
-  (when-let [errors (first (st/validate body schema))]
-    (->> errors
-         (map (fn [[k v]] [k [v]]))
-         (into {}))))
+(defn validate-body
+  "If valid then returns nil and sanitized body otherwise list of errors and partial body"
+  [body schema]
+  (st/validate body schema {:strip true}))
 
 (comment
 
+  (st/validate {:password "342323"} login-user-schema)
+;; => [{:email "can't be blank", :password "is too short (minimum is 8 character)"} {}]
+
+  (st/validate {:email nil :password "342323"} login-user-schema)
+;; => [{:email "can't be blank", :password "is too short (minimum is 8 character)"} {}]
+
+  (st/validate {:email "" :password "342323"} login-user-schema)
+;; => [{:email "can't be blank", :password "is too short (minimum is 8 character)"} {}]
+
   (st/validate {:email "1" :password "342323"} login-user-schema)
-  ;; => [nil {:email "1@email.com", :password "12321342323"}]
+;; => [{:email "is invalid", :password "is too short (minimum is 8 character)"} {}]
 
   (st/validate {:email "1@emailcom" :password "12321342323"} login-user-schema)
-  ;; => [{:email "must be a valid email"} {:password "12321342323"}]
+;; => [{:email "is invalid"} {:password "12321342323"}]
 
   (st/validate {:email "1@emailcom" :password "2323"} login-user-schema)
-  ;; => [{:email "must be a valid email", :password "less than the minimum 8"} {}]
+;; => [{:email "is invalid", :password "is too short (minimum is 8 character)"} {}]
 
-  (validate-body {:email "1@emailcom" :password "2323"} login-user-schema)
-  ;; => {:email ["must be a valid email"], :password ["less than the minimum 8"]}
+  (st/validate {:email "s123@email.com" :password "232323343"} login-user-schema)
+;; => [nil {:email "s123@email.com", :password "232323343"}]
+
+  (st/validate {:str " 1 2 3 4 5 " :foo "sdfdsf"} [[:str st/required {:coerce s/trim} st/string [st/min-count 5]]] {:strip true})
+;; => [nil {:str "1 2 3 4 5"}]
+
+  (validate-body {:email "1@emailcom" :password "23233"} login-user-schema)
+;; => [{:email "is invalid", :password "is too short (minimum is 8 character)"} {}]
 
   (validate-body {:email "1@email.com" :password "2323123213"} login-user-schema)
-  ;; => nil
-
-  (map (fn [[k v]] [k [v]]) {:email "must be a valid email", :password "less than the minimum 8"})
-  ;; => ([:email ["must be a valid email"]] [:password ["less than the minimum 8"]])
+;; => [nil {:password "2323123213", :email "1@email.com"}]
 
   )
